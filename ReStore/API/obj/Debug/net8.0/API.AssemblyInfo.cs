32ccfabb89f1f79b0b46633a996481ee9823076a
Diwakar Singh <the.walkersingh@hotmail.com>
2024-02-27 15:21:43 +0530

    API Build Completed

--- ReStore/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ ReStore/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adfd97a402ffd844c9f471498696799f44bf7fa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76be594a57b10d12f74d49cd771931b9be4d77f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
