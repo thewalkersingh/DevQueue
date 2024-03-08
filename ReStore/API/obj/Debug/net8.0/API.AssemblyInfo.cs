79b9d75fecfec0a3954b9a17cf1bb31c3de2ec10
Diwakar Singh <the.walkersingh@hotmail.com>
2024-03-08 16:05:21 +0530

    API Updated

--- ReStore/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ ReStore/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199466d3220aefdb888c1b8bb83c670358c41e16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd34ea48a918d28760ac5646fe1855b21d4bbc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
