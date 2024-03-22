6704350161e8766e8649d1505777a767eff6af33
Diwakar Singh <the.walkersingh@hotmail.com>
2024-03-22 14:02:24 +0530

    dotnet ef migrations add BasketEntityAdded

--- ReStore/API/Data/StoreContext.cs
+++ ReStore/API/Data/StoreContext.cs
@@ -8,4 +8,5 @@ public class StoreContext : DbContext
     public StoreContext(DbContextOptions options) : base(options)
     { }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Basket> Baskets { get; set; } 
 }
